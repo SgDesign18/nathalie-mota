bf3f85805989484bbc8ff552d9a003bca08a17fe
Sandrine Gaston <sg.design11@gmail.com>
2024-03-26 11:25:13 +0100

    template vide

---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer>
+
+wp_nav_menu(array(
+    'theme_location' => 'menu-footer',
+    'menu_class' => 'menu-footer', 
+));
+
+</footer>
+    <?php wp_footer(); ?>
+</body>
+</html>
---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php
+// Enqueue stylesheet - Ajout de mon style.css
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'theme-style', get_stylesheet_directory_uri() . '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+
+
+add_theme_support('title-tag');
+
+
+function register_menus() {
+    register_nav_menus(
+        array(
+            'primary' => __( 'Menu Principal' ),
+            'menu-footer' => __( 'Menu Footer' )
+        )
+    );
+}
+add_action( 'init', 'register_menus' );
+
